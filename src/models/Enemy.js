b22a0dc0d1b888dd3a586e866dcfd0435a6f0f4f
Adam Snow <adamjohnsnow@icloud.com>
2017-06-07 18:16:10 +0100

    angular HP display in Battle

--- src/models/Enemy.js
+++ src/models/Enemy.js
@@ -1,7 +1,7 @@
 var Enemy = function(){
   this.attack = Math.floor((Math.random() * 10) +1)
   this.defence = Math.floor((Math.random() * 10) +1)
-  this.hp = 100
+  this.hp = 90
   this.name = "Darth Vadar"
 };
 
