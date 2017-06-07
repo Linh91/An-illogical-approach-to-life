7869c3332b4df79d83dfef52f52489210dd967ae
Adam Snow <adamjohnsnow@icloud.com>
2017-06-07 13:24:17 +0100

    fix localhost issues in tests

--- test/battleFeatureSpec.js
+++ test/battleFeatureSpec.js
@@ -4,13 +4,12 @@ var assert = require('assert');
 
 var app = require('../server/index.js')
 
-Browser.localhost('localhost', 9000);
 
 describe('Battle', function(done) {
   const browser = new Browser();
 
   it('can start a battle', function(done) {
-    browser.visit('/battle').then(function() {
+    browser.visit('http://localhost:9000/battle').then(function() {
       assert.ok(browser.success);
       browser.assert.text('h2', 'BATTLE');
       browser.assert.text('p', 'Test Warrior');
