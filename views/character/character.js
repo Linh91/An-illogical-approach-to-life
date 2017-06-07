1c62f851036e8c6028fde1f42def5bb995c4c4d4
Sami <sami@Samis-MacBook-Pro.local>
2017-06-07 12:57:17 +0100

    character creation

---
+++ views/character/character.js
@@ -0,0 +1,12 @@
+var app = angular.module('createCharacter', []);
+
+app.controller('character', function($scope){
+  $scope.create = function(){
+  console.log('sup')
+  }
+})
+// var hero = new Character();
+// hero.name = 'Test Warrior';
+// hero.userId = 'ABC1';
+// hero.avatar = '/creature.jpg';
+// hero.save
