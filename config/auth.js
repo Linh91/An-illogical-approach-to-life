631b094a9814511e190bf2de6b5494e2a84330f7
Sami <sami@Samis-MacBook-Pro.local>
2017-06-07 12:05:52 +0100

    simple form added for character in angular

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+   'facebookAuth' : {
+       'clientID'      : '226543737840328', // your App ID
+       'clientSecret'  : '940a1158710a4441774d3fb06a8c7485', // your App Secret
+       'callbackURL'   : 'http://localhost:9000/auth/facebook/callback'
+   },
+
+   'twitterAuth' : {
+       'consumerKey'       : 'SrK3QwOJ2b86geoTshPRhds2n',
+       'consumerSecret'    : '60zmfa2lOv4xti5PW5MdypoBG45fnlwbgV1BSrs5EBIbq3CqSV',
+       'callbackURL'       : 'http://localhost:9000/auth/twitter/callback'
+   },
+
+   'googleAuth' : {
+       'clientID'      : '655768373281-69dlgllinf3tg846mqd5n28nh6m5g1uh.apps.googleusercontent.com',
+       'clientSecret'  : 'xFwVcxZvUdkmzvnNRJGafMeb',
+       'callbackURL'   : 'http://localhost:9000/auth/google/callback'
+   }
+
+};
