6610b3b8eef645dd1229b80605239fe962680bce
Adam Snow <adamjohnsnow@icloud.com>
2017-06-08 14:18:42 +0100

    end game happens

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const path = require('path')
 var database = require('./config/database.js')
 var User = require('./src/models/User');
+var EndGame = require('./src/models/EndGame');
+var Rewards = require('./src/models/Rewards');
 var Attack = require('./src/models/Attack');
 var Heal = require('./src/models/Heal');
 var Character = require('./src/models/Character');
@@ -121,17 +123,25 @@ app.get('/new-battle', function(req, res){
   sess = req.session
   enemy = new Enemy();
   Character.find({ }, function(err, characters) {
-    sess.battle = new Battle(characters[1], enemy)
+    sess.hero = characters[1];
+    sess.battle = new Battle(sess.hero, enemy)
     res.redirect('/battle')
   })
 })
 
 app.get('/battle', function(req, res){
+  var checkEndGame = new EndGame(req.session.battle)
+  if (req.session.battle.outcome == 'won') {
+    res.redirect('/win')
+  } else if (req.session.battle.outcome == 'lost'){
+    res.redirect('/lose')
+  } else {
     res.render('battle/battle', {
       lastGo: req.session.lastGo,
       battle: req.session.battle
     })
-  })
+  }
+})
 
 app.post('/attack', function(req, res) {
   var attack = new Attack(req.session.battle.firstPlayer, req.session.battle.secondPlayer);
@@ -143,6 +153,18 @@ app.post('/heal', function(req, res) {
   var heal = new Heal(req.session.battle.firstPlayer);
   req.session.lastGo = heal.outcome
   res.redirect('/battle')
+})
+
+app.get('/win', function(req, res) {
+  var reward = new Rewards(req.session.battle.firstPlayer)
+  sess.hero.save();
+  res.render('battle/win',{
+    reward: reward
+  })
+})
+
+app.get('/lose', function(req, res) {
+  res.render('battle/lose')
 })
 
 app.listen(PORT, () => {
--- src/models/Battle.js
+++ src/models/Battle.js
@@ -5,14 +5,4 @@ var Battle = function(player1, player2){
   this.outcome = null
 }
 
-
-
-Battle.prototype.checkForEndGame = function(){
-  if(this.secondPlayer.hp <= 0){
-    this.gameOver = true; this.outcome = 'won'
-  }else if(this.firstPlayer.hp <= 0) {
-    this.gameOver = true; this.outcome = 'lost'
-  }
-}
-
 module.exports = Battle;
